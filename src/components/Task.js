9157ead1b4e32f66b3e3811fb7a933af870faff8
unknown <vaishnav.vsft@gmail.com>
2021-05-11 20:46:36 +0530

    Removed CSS file

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './task.css'
 import DeleteIcon from '@material-ui/icons/Delete';
 import CheckIcon from '@material-ui/icons/Check';
 import firebase from '../util/firebase'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import EditIcon from '@material-ui/icons/Edit';
+
 
 function Task({task, inprogress, id}) {
+
+
+    const [edit, setEdit] = useState("");
 
     function toggleInProgress(){
         firebase.firestore().collection("todos").doc(id).update({
@@ -17,9 +22,15 @@ function Task({task, inprogress, id}) {
         firebase.firestore().collection("todos").doc(id).delete();
     }
 
+    function handleEdit() {
+
+    }
+
     return (
         <div className="main-content">
             {task}
+
+            <EditIcon className="small-icon" />
             <DeleteIcon className="small-icon" onClick={deleteTodo}/>
 
             {inprogress ? <CheckIcon className="small-icon" onClick={toggleInProgress}/> : <ChevronLeftIcon className="small-icon" onClick={toggleInProgress} /> }
